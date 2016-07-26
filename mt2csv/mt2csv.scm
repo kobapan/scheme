d6bc2f075f6cd53603cd5fe43c7d857534b9b95e
kobapan <kobapan@gmail.com>
2016-07-27 00:19:54 +0900

    cond -> when

--- mt2csv/mt2csv.scm
+++ mt2csv/mt2csv.scm
@@ -13,21 +13,20 @@
 (define %make-entry
   (lambda (iport body?)
     (let1 line (read-line iport)
-      (cond ((eof-object? line) #f)
-            (else
-             (let1 entry (%sanitize line) 
-               (cond ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
-                     ((string-scan entry "PRIMARY CATEGORY: ") #f)
-                     ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
-                     ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
-                     ((string-scan entry "BODY:") (display "\"") (body? #t))
-                     ((string-scan entry "-----") (when (body?) (body? #f)))
-                     ((string-scan entry "--------") ; end of 1 post
-                      (display "\"")
-                      (newline (current-output-port)) )
-                     ((body?) (print entry))
-                     (else #f))
-               (%make-entry iport body?) ))))))
+      (when (not (eof-object? line))
+        (let1 entry (%sanitize line) 
+          (cond ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
+                ((string-scan entry "PRIMARY CATEGORY: ") #f)
+                ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
+                ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
+                ((string-scan entry "BODY:") (display "\"") (body? #t))
+                ((string-scan entry "-----") (when (body?) (body? #f)))
+                ((string-scan entry "--------") ; end of 1 post
+                 (display "\"")
+                 (newline (current-output-port)) )
+                ((body?) (print entry))
+                (else #f))
+          (%make-entry iport body?) )))))
 
 (define %doit
   (lambda (iport)
