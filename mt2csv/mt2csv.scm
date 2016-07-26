1fee8e34365a012eb59c94ed53555f453df0e5a8
kobapan <kobapan@gmail.com>
2016-07-27 00:08:43 +0900

    body flag をグローバル変数から、クロージャに変更

--- mt2csv/mt2csv.scm
+++ mt2csv/mt2csv.scm
@@ -6,30 +6,51 @@
 
 (use util.match)
 
-(define %body-flag #f)
-
 (define %sanitize
   (lambda (in)
     (regexp-replace-all "\"" in "'") ))
 
 (define %make-entry
-  (lambda (line)
-    (let1 entry (%sanitize line) 
-      (cond ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
-            ((string-scan entry "PRIMARY CATEGORY: ") #f)
-            ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
-            ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
-            ((string-scan entry "BODY:") (set! %body-flag #t) (display "\""))
-            ((string-scan entry "-----") (when %body-flag (set! %body-flag #f)))
-            ((string-scan entry "--------") ; end of 1 post
-             (display "\"")
-             (newline (current-output-port)) )
-            (%body-flag (print entry))
-          (else #f)) )))
+  (lambda (iport body?)
+    (let1 line (read-line iport)
+      (cond ((eof-object? line) #f)
+            (else
+             (let1 entry (%sanitize line) 
+               (cond ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
+                     ((string-scan entry "PRIMARY CATEGORY: ") #f)
+                     ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
+                     ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
+                     ((string-scan entry "BODY:") (display "\"") (body? #t))
+                     ((string-scan entry "-----") (when (body?) (body? #f)))
+                     ((string-scan entry "--------") ; end of 1 post
+                      (display "\"")
+                      (newline (current-output-port)) )
+                     ((body?) (print entry))
+                     (else #f))
+               (%make-entry iport body?) ))))))
 
 (define %doit
   (lambda (iport)
-    (port-for-each %make-entry (lambda () (read-line iport))) ))
+    (%make-entry iport 
+                 (let1 flag #f
+                   (lambda args
+                     (cond ((pair? args) (set! flag (car args)))
+                           (else flag) ))))))
+
+
+;; クロージャ ← 環境（この場合はlet）の中で手続きを宣言する。静的スコープ
+;; (define body?
+;;   (let ((flag #f))
+;;     (lambda args
+;;       (cond ((pair? args) (set! flag (car args)))
+;;             (else flag) ))))
+;;
+;; 下記はよく似ているように思うが、手続きの中でletが呼ばれている。これは手続きの環境にはならない。
+;; (define body?
+;;   (lambda args
+;;     (let ((flag #f))
+;;       (cond ((pair? args) (set! flag (car args)))
+;;             (else flag) ))))
 
 (define main
   (lambda (args)
