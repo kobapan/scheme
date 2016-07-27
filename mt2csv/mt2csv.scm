19324e0ea7f6c41f406ac8cb137464218ace5461
kobapan <kobapan@gmail.com>
2016-07-27 13:16:28 +0900

    %make-entry に渡す手続きを抽象化

--- mt2csv/mt2csv.scm
+++ mt2csv/mt2csv.scm
@@ -10,31 +10,34 @@
   (lambda (in)
     (regexp-replace-all "\"" in "'") ))
 
+(define on?
+  (let1 flag #f
+    (lambda args
+      (cond ((null? args) flag)
+            (else (set! flag (car args))) ))))
+
 (define %make-entry
-  (lambda (iport body?)
+  (lambda (iport body-on?)
     (let1 line (read-line iport)
       (unless (eof-object? line)
         (let1 entry (%sanitize line) 
-          (cond ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
-                ((string-scan entry "PRIMARY CATEGORY: ") #f)
-                ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
-                ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
-                ((string-scan entry "BODY:") (display "\"") (body? #t))
-                ((string-scan entry "-----") (when (body?) (body? #f)))
-                ((string-scan entry "--------") ; end of 1 post
-                 (display "\"")
-                 (newline (current-output-port)) )
-                ((body?) (print entry))
-                (else #f))
-          (%make-entry iport body?) )))))
+          (cond 
+           ((string-scan entry "TITLE: " 'after) => (cut format #t "\"~a\"\," <>))
+           ((string-scan entry "PRIMARY CATEGORY: ") #f)
+           ((string-scan entry "CATEGORY: " 'after) => (cut format #t "\"~a\"\," <>))
+           ((string-scan entry "DATE: " 'after) => (cut format #t "\"~a\"\," <>))
+           ((string-scan entry "BODY:") (display "\"") (body-on? #t))
+           ((string-scan entry "-----") (when (body-on?) (body-on? #f)))
+           ((string-scan entry "--------") ; end of 1 post
+            (display "\"")
+            (newline (current-output-port)) )
+           ((body-on?) (print entry))
+           (else #f) )
+          (%make-entry iport body-on?) )))))
 
 (define %doit
   (lambda (iport)
-    (%make-entry iport 
-                 (let1 flag #f
-                   (lambda args
-                     (cond ((pair? args) (set! flag (car args)))
-                           (else flag) ))))))
+    (%make-entry iport on?) ))
 
 (define main
   (lambda (args)
@@ -45,3 +48,4 @@
       (else
        (print #`"Usage: ,(car args) <ifile> <ofile>\n")
        (exit 0) ))))
+
