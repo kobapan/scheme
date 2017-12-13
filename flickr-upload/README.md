fad747fa4fe3758ad1eaafaf41569fcaf550580d
kobapan <kobapan@gmail.com>
2017-12-13 20:08:57 +0900

    update

--- flickr-upload/README.md
+++ flickr-upload/README.md
@@ -3,19 +3,24 @@
 FlickrのコマンドラインAPI、flickcurlが便利なので、複数ファイルを一度にアップロードするためのラッパープログラムを作りました。
 
 # flickcurl
-- [Flickcurl公式](http://librdf.org/flickcurl/)
-- [flickcurlのインストール](https://www20.atwiki.jp/kobapan/pages/332.html#id_53b77e44)
+[Flickcurl公式](http://librdf.org/flickcurl/)
+
+事前にflickcurlをインストール＆設定しておく必要があります。
+→ [Flickr/flickcurl-コマンドラインでFlickr](https://www20.atwiki.jp/kobapan/pages/332.html#id_53b77e44)
 
 # 使い方
-$ flickr-upload.scm FLICKR-USERNAME INFILE  
-ID[NNN] TITLE[name] URL[xxx.jpg]  
-.  
-.  
-.
+> $ flickr-upload.scm FLICKR-USERNAME INFILE  
+> ID[NNN] TITLE[name] URL[xxx.jpg]  
+> .  
+> .  
+> .  
+flickrのユーザ名と、アップロードする画像ファイルのパス名を記述したファイル名を、引数にとります。
+アップロードした画像のflickrでのIDと、タイトル、URLを返します。 
 
 ## INFILEの構文
-image-path[ title]  
-.  
-.  
-.
+1行に一つの画像ファイルを記述します。スペースに続いて画像タイトルを記述します（省略可）。 
+> /path/to/image.(jpg|jpeg|png|gif)[ title]  
+> .  
+> .  
+> .
 
--- flickr-upload/flickr-upload.scm
+++ flickr-upload/flickr-upload.scm
@@ -3,6 +3,7 @@
 ;; flickcurlのラッパー
 ;; Flickcurl: C library for the Flickr API <http://librdf.org/flickcurl/>
 ;;
+
 ;; 1引数にファイル名を取り
 ;; ファイル内に1行ずつ記述された画像ファイルを
 ;; flickcurlに渡して、flickrにアップロードし
@@ -13,17 +14,30 @@
 ;; rxmatch-let <http://practical-scheme.net/gauche/man/gauche-refj/Zheng-Gui-Biao-Xian-.html#index-rxmatch_002dpositions>
 ;; コマンドの出力を取る <https://practical-scheme.net/wiliki/wiliki.cgi?Scheme%3A%E3%83%86%E3%82%AD%E3%82%B9%E3%83%88%E5%87%A6%E7%90%86#H-mocs7o>
 ;;
-;; rxmatch-let や rxmatch-if の使い方がいまいち
+
+;; USAGE
+;; $ flickr-upload.scm <flickr-id> <infile>
+;; 
+;; infile's syntax
+;; --------
+;; /path/to/image.(jpg|jpeg|png|gif)[ title]
+;; .
+;; .
+;; .
+;; ----
+
 
 (use util.match); match
 (use gauche.process); process-output->string,process-output->string-list
 
 (define usage
-  (lambda ()
+  (lambda (err)
+    (when err (print "ERROR: " err))
     (print #"Usage: ~(sys-basename *program-name*) <flickr-id> <infile>
-infile
+
+infile's syntax
 --------
-image-path[ title]
+/path/to/image.(jpg|jpeg|png|gif)[ title]
 .
 .
 .
@@ -31,10 +45,9 @@ image-path[ title]
 ")
     (exit 2) ))
 
-
-;; ID で flickrdf を呼び出して
+;; UID と ID で flickrdf を呼び出して
 ;; flickr画像のurlを取得して
-;; URL[~a]\nを現在の標準出力に印字
+;; "URL[photoURL]\n"を現在の標準出力に印字
 (define %geturl
   (lambda (uid id)
     (format #t "URL[~a]\n"
@@ -42,48 +55,38 @@ image-path[ title]
                #"flickrdf http://www.flickr.com/photos/~|uid|/~|id|/\
  | grep '\"Medium\"' | grep -o 'https://.*jpg'") )))
 
-;; img-path と title で  flickcurl upload を呼び出して
-;; ID[photoID] TITLE[title] を現在の標準出力に印字
+;; img-path と title で flickcurl upload を呼び出して
+;; "ID[photoID] TITLE[photoTITLE] "を現在の標準出力に印字
 ;; IDを返す
 (define %upload
   (lambda (img-path title)
-    (let* ((res (process-output->string-list
-                 #"flickcurl upload \"~|img-path|\" title \"~|title|\" public"))
-           (id (rxmatch->string #/: (.+)/ (caddr res) 1)))
-      (format #t "ID[~a] TITLE[~a] " id title)
+    (let* ((up-title (if title (format #f "title \"~a\"" title) ""))
+           (res (process-output->string-list
+                 #"flickcurl upload \"~|img-path|\" ~|up-title| public") )
+           (id (rxmatch->string #/: (.+)/ (caddr res) 1)) )
+      (format #t "ID[~a] TITLE[~a] " id (if title title ""))
       id )))
 
+;; 現在の入力ポートから1行ずつ読み取り
 ;; 画像ファイルパスとタイトルを
 ;; %uploadに渡して帰ったIDを
-;; %geturlに渡す
-(define %call
-  (lambda (uid file title)
-    (%geturl uid (%upload file title))))
-
-;; 現在の入力ポートから1行ずつ読み取り
-;; UIDと画像ファイルパスとタイトルを
-;; %call に渡して
+;; %geturlに渡して
 ;; 自身を呼び出す
 (define %do
   (lambda (uid)
     (let1 line (read-line)
       (unless (eof-object? line)
-        (rxmatch-cond
-         ((#/([^ ]+\.(jpg|png|gif)) ([^ ]+)/ line)
-          (#f file #f title)
-          (%call uid file title) )
-         ((#/([^ ]+\.(jpg|png|gif))/ line)
-          (#f file #f)
-          (%call uid file (sys-basename file)) )
-         (else (usage)) )
+        (rxmatch-if (#/([^ ]+\.(jpg|jpeg|png|gif))( ([^ ]+))?/ line)
+          (#f file #f #f title)
+          (%geturl uid (%upload file title))
+         (usage "file format error.") )
         (%do uid) ))))
 
 (define main
   (lambda (args)
     (match args
       ((_ user infile)
-       (with-input-from-file infile
-                      (lambda () (%do user) )))
+       (with-input-from-file infile (lambda () (%do user) )))
       (else
-       (usage) ))
+       (usage "args error.") ))
     0 ))
